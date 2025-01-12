0bccb85eb903abaa12c8b95e69e5bb8f3c276e2a
Narjes Abu Rabia <narjes.rabia2@gmail.com>
2025-01-12 13:48:05 +0200

    updatetest

--- tests/e2e/e2e_test.py
+++ tests/e2e/e2e_test.py
@@ -1,52 +1,46 @@
+
+import pytest
+import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-driver = webdriver.Chrome() 
-driver.get("http://127.0.0.1:8080/#/register?redirect=/")
-driver.set_window_size(1280, 712)
-
-try:
-
-    name_field = driver.find_element(By.ID, "name")
-    name_field.click()
-    name_field.send_keys("lara")
-    
-
-    email_field = driver.find_element(By.ID, "email")
-    email_field.click()
-    email_field.send_keys("lara33@gmail.com")
-    
-
-    password_field = driver.find_element(By.ID, "password")
-    password_field.click()
-    password_field.send_keys("lara")
-    
-
-    confirm_password_field = driver.find_element(By.ID, "passwordConfirm")
-    confirm_password_field.click()
-    confirm_password_field.send_keys("lara")
-    
+class TestDefaultSuite():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
   
-    register_button = driver.find_element(By.CSS_SELECTOR, ".mt-3")
-    register_button.click()
-    
-   
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "username"))
-    )
-    username = driver.find_element(By.ID, "username").text
-    assert username == "lara", f"Expected username 'lara', but got {username}"
-    
-    print("Test passed successfully!")
-
-except Exception as e:
-    print(f"Test failed: {e}")
-
-finally:
-    driver.quit()
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_signuptest(self):
+    self.driver.get("http://127.0.0.1:8080/#/register?redirect=/")
+    self.driver.set_window_size(1280, 712)
+    self.driver.find_element(By.ID, "name").click()
+    self.driver.find_element(By.ID, "name").send_keys("lian")
+    self.driver.find_element(By.ID, "email").click()
+    self.driver.find_element(By.ID, "email").send_keys("lian33@gmail.com")
+    self.driver.find_element(By.ID, "password").click()
+    self.driver.find_element(By.ID, "password").send_keys("lian")
+    self.driver.find_element(By.ID, "passwordConfirm").click()
+    self.driver.find_element(By.ID, "passwordConfirm").send_keys("lian")
+    self.driver.find_element(By.CSS_SELECTOR, ".mt-3").send_keys(Keys.ENTER)
+  
+  def test_signuptest(self):
+    self.driver.get("http://127.0.0.1:8080/#/register?redirect=/")
+    self.driver.set_window_size(1280, 712)
+    self.driver.find_element(By.ID, "name").click()
+    self.driver.find_element(By.ID, "name").send_keys("rina")
+    self.driver.find_element(By.ID, "email").click()
+    self.driver.find_element(By.ID, "email").send_keys("rina33@gmail.com")
+    self.driver.find_element(By.ID, "password").click()
+    self.driver.find_element(By.ID, "password").send_keys("rina")
+    self.driver.find_element(By.ID, "passwordConfirm").click()
+    self.driver.find_element(By.ID, "passwordConfirm").send_keys("rina")
+    self.driver.find_element(By.CSS_SELECTOR, ".mt-3").send_keys(Keys.ENTER)
+  
